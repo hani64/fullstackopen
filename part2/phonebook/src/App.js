be0daeb7a960188fc70faa487713bf0bc733e9f6
Hani Asim <haniasim@gmail.com>
2023-01-18 21:20:53 -0500

    Finished Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,25 @@
-import { useReducer, useState } from "react"
+import { useEffect, useReducer, useState } from 'react'
+import axios from 'axios'
 
-import Filter from "./components/Filter"
-import PersonForm from "./components/PersonForm"
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "39-44-5323523", id: 1 },
-  ])
-  const [filter, setFilter] = useState("")
+  const [persons, setPersons] = useState([])
+  const [filter, setFilter] = useState('')
   const [newEntry, setNewEntry] = useReducer(
     (prev, next) => {
       return { ...prev, ...next }
     },
-    { name: "", phone: "" }
+    { name: '', phone: '' }
   )
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((res) => {
+      console.log(res.data)
+      setPersons(res.data)
+    })
+  }, [])
 
   const addName = (e) => {
     e.preventDefault()
@@ -22,10 +27,10 @@ function App() {
 
     if (names.includes(newEntry.name)) {
       alert(`${newEntry.name} is already added to phonebook`)
-      setNewEntry({ name: "", phone: "" })
+      setNewEntry({ name: '', phone: '' })
       return
     }
-    if (newEntry.name === "" || newEntry.phone === "") {
+    if (newEntry.name === '' || newEntry.phone === '') {
       alert(`Please enter something for both fields`)
       return
     }
@@ -33,9 +38,8 @@ function App() {
       ...persons,
       { name: newEntry.name, phone: newEntry.phone, id: persons.length + 1 },
     ])
-    setNewEntry({ name: "", phone: "" })
+    setNewEntry({ name: '', phone: '' })
   }
-
   const changeName = (e) => setNewEntry({ name: e.target.value })
   const changePhone = (e) => setNewEntry({ phone: e.target.value })
   const changeFilter = (e) => setFilter(e.target.value)
@@ -45,7 +49,7 @@ function App() {
       <h2>Phonebook</h2>
       <Filter {...{ filter, changeFilter }} />
       <h2>add a new</h2>
-      <PersonForm {...{ addName, newEntry, changeName, changePhone }}/>
+      <PersonForm {...{ addName, newEntry, changeName, changePhone }} />
       <h2>Numbers</h2>
       <Persons {...{ persons, filter }} />
     </div>
